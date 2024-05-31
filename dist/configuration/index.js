824444d0259305df246eb381cd6c4bde4025f3c4
Mister@Developer <boliwiy332@extemer.com>
2024-05-31 17:50:31 +0530

    edit configuration

--- dist/configuration/index.js
+++ dist/configuration/index.js
@@ -4,6 +4,6 @@ exports.MONGODB_URI = exports.PORT = exports.HOST = void 0;
 const dotenv_1 = require("dotenv");
 (0, dotenv_1.config)();
 exports.HOST = process.env.HOST || 'localhost';
-exports.PORT = Number(process.env.PORT) || 3000;
+exports.PORT = Number(process.env.POST) || 3000;
 exports.MONGODB_URI = process.env.MONGODB_URI || '';
 //# sourceMappingURL=index.js.map
